6be7fd2e6704fc356aa728b0ed04ab0cb14c7d6c
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-06-29 13:28:34 +0530

    Generates Controller for Commercial Units

---
+++ app/controllers/commercial_units_controller.rb
@@ -0,0 +1,13 @@
+class CommercialUnitsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :houses
   resources :complex_buildings
+  resources :commercial_units
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
