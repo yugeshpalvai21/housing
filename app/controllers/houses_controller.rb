5f2b3a3efe0a4124fa1d69776119b9681a25cce6
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-06-30 00:47:20 +0530

    Implements buy action for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,5 @@
 class HousesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :buy]
   before_action :autherize_manager, only: [:new, :create, :edit, :update, :destroy]
   
   def index
@@ -43,6 +43,16 @@ class HousesController < ApplicationController
     redirect_to houses_path, notice: 'House Removed Successfully'
   end
 
+  def buy
+    if current_user.manager
+      redirect_to root_path, notice: 'As a Manager youre not allowed to buy this home'
+    else
+      @house = House.find(params[:id])
+      very_long_task
+      redirect_to @house, notice: 'Congratulations on Your New House!!!'
+    end
+  end
+
   private
 
   def house_params
@@ -53,5 +63,10 @@ class HousesController < ApplicationController
     unless current_user.manager
       redirect_to root_path, notice: 'Restricted Action'
     end
+  end
+
+  def very_long_task
+    sleep 60
+    true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#welcome'
-  resources :houses
-  resources :complex_buildings
-  resources :commercial_units
+  resources :houses do
+    member do
+      get 'buy'
+    end
+  end
+
+  resources :complex_buildings do
+    member do
+      get 'buy'
+    end
+  end
+
+  resources :commercial_units do
+    member do
+      get 'buy'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
