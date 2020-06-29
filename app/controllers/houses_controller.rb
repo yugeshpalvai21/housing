1ede7f8bf16ae325ea1dd21f5064569a289e1d2f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-06-29 15:58:11 +0530

    Implements House Controller Actions and Views

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,13 +1,48 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.order('created_at DESC')
   end
 
   def show
+    @house = House.find(params[:id])
   end
 
   def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to @house, notice: 'New House Created Successfully'
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    if @house.update(house_params)
+      redirect_to @house, notice: 'Updated Successfully'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path, notice: 'House Removed Successfully'
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:owner, :address, :rooms, :sqmt, :floors, :air_cond, :price)
   end
 end
